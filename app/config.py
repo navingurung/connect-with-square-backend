6ca5cdb304197892ab12f3179fe3c3157351ef4f
navingurung <navin.gurung@tai-matsu.jp>
2026-05-15 16:33:38 +0900

    update config.py

--- app/config.py
+++ app/config.py
@@ -11,17 +11,17 @@ class Settings(BaseSettings):
     APP_NAME: str = "Square Samurai Test Backend"
     DEBUG: bool = True
 
-    DATABASE_URL: str = "postgresql://postgres:postgres@postgres:5432/square_samurai_test"
+    DATABASE_URL: str = "postgresql://square_samurai_test_user:xkbujqFOTshuQPkcAtLCPymhTwWKLy5Z@dpg-d83cg00js32c73ca5qa0-a/square_samurai_test"
 
     SQUARE_ENVIRONMENT: str = "sandbox"
     SQUARE_BASE_URL: str = "https://connect.squareupsandbox.com"
     SQUARE_APPLICATION_ID: str
     SQUARE_APPLICATION_SECRET: str
-    SQUARE_REDIRECT_URI: str = "http://localhost:8800/api/square/oauth/callback"
+    SQUARE_REDIRECT_URI: str = "https://connect-with-square-backend.onrender.com/api/square/oauth/callback"
     SQUARE_VERSION: str = "2026-01-22"
     SQUARE_SCOPES: str = "MERCHANT_PROFILE_READ PAYMENTS_READ ORDERS_READ"
 
-    FRONTEND_URL: str = "http://localhost:3000"
+    FRONTEND_URL: str = "https://connect-with-square-frontend.onrender.com/"
 
     @property
     def square_scopes_list(self) -> list[str]:
